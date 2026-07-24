16dfa52a1e54f55e70a08985620b550a0b78c631
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-07-24 19:38:00 +0100

    chore: add STATIC_ROOT configuration to settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', cast=bool, default=DEBUG)
