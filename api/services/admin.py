2061f7aff949ca1087114dd10a6f44535974ac14
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-07-24 20:12:12 +0100

    feat: register portfolio models in the Django admin interface

--- api/services/admin.py
+++ api/services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Experience, Service, CategoriBlog, Blog, Temoignage,
+    Contact, CategorieProjet, Projet, Profil
+)
 
-# Register your models here.
+admin.site.register(Experience)
+admin.site.register(Service)
+admin.site.register(CategoriBlog)
+admin.site.register(Blog)
+admin.site.register(Temoignage)
+admin.site.register(Contact)
+admin.site.register(CategorieProjet)
+admin.site.register(Projet)
+admin.site.register(Profil)
